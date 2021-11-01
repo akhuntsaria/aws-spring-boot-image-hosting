9a1d2db101254ac035d6c8ea390c80410a05eec2
Akaki Khuntsaria <akakikhuntsaria97@gmail.com>
2021-11-01 23:24:39 +0400

    Add upload action, request; Restrict extensions

--- src/main/java/com/github/akhuntsaria/imagehosting/controller/HomeController.java
+++ src/main/java/com/github/akhuntsaria/imagehosting/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.github.akhuntsaria.imagehosting.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
@@ -9,5 +11,10 @@ public class HomeController {
     @GetMapping
     public String home() {
         return "home";
+    }
+
+    @PostMapping
+    public RedirectView upload(@RequestParam("file") MultipartFile file) {
+        return new RedirectView("/");
     }
 }
